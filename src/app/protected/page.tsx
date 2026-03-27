2dd7f3d80e57987add0470e99d72d994d54f72c7
RizuMong <rizkihaddiprayoga@gmail.com>
2026-03-28 04:31:59 +0700

    fix add npm start

--- src/app/protected/page.tsx
+++ src/app/protected/page.tsx
@@ -9,7 +9,7 @@ export default async function ProtectedPage() {
   const { data, error } = await supabase.auth.getClaims()
   if (error || !data?.claims) {
     redirect('/auth/login')
-  }
+  };
 
   return (
     <div className="flex h-svh w-full items-center justify-center gap-2">
