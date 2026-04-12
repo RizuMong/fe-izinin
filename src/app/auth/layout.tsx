14181494b482aa5760495da84f7e2ab48cbae356
RizuMong <rizkihaddiprayoga@gmail.com>
2026-04-12 12:25:13 +0700

    update auth layout

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useUserStore } from "@/store/user.store"
-import { useRouter } from "next/navigation"
+import { useRouter, useSearchParams, usePathname } from "next/navigation"
 import { useEffect } from "react"
 
 export default function AuthLayout({
@@ -11,14 +11,29 @@ export default function AuthLayout({
 }) {
   const { user, isLoading } = useUserStore()
   const router = useRouter()
+  const searchParams = useSearchParams()
+  const pathname = usePathname()
+
+  const type = searchParams.get("type")
+  const accessToken = searchParams.get("access_token")
+
+  const isRecovery =
+    type === "recovery" || !!accessToken
+
+  const isResetPage = pathname.includes("reset-password")
 
   useEffect(() => {
-    if (!isLoading && user) {
+    if (isLoading) return
+
+    if (isResetPage && !isRecovery) {
+      router.replace("/login")
+      return
+    }
+
+    if (user && !isRecovery) {
       router.replace("/time-off/request")
     }
-  }, [user, isLoading, router])
-
-  if (isLoading) return null
+  }, [user, isLoading, isRecovery, isResetPage, router])
 
   return <>{children}</>
 }
